3bec86e270564a704a3860321e5a1c41da59d12c
vtomilin <vtomilin@myopera.com>
2014-02-24 17:30:46 -0500

    Some progress

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,8 @@ import path = require('path');
 class AsdkTypedApp extends event.EventEmitter {
     private running: boolean = false;
     private nextWorker: number = 0;
+    private pendingItems: number = 0; // Items, pending processing
+    private outputFile: number = -1;
 
     /**
      * Initializes the instance with given SDK path, output path,
@@ -29,7 +31,7 @@ class AsdkTypedApp extends event.EventEmitter {
             throw "'" + this.sdkpath + "' does not exist";
         }
 
-        this.on('start-processing', () => this.processDir(this.sdkpath));
+        this.on('start-processing', this.process.bind(this));
     }
 
     run(): void {
@@ -41,6 +43,7 @@ class AsdkTypedApp extends event.EventEmitter {
 
         cluster.setupMaster({
             exec: './js/worker.js',
+            args: [this.sdkpath],
             silent: true
         });
 
@@ -49,16 +52,45 @@ class AsdkTypedApp extends event.EventEmitter {
                 // At least some came up online, continuing then
                 this.emit('start-processing');
             } else {
-                this.emit('error', 'Failed to start workers');
+                this.emit('end', {type: 'error', data: 'Failed to start workers'});
             }
         }, this.workersOnlineTimeoutMs);
 
         cluster.on('online', () => ++onlineWorkers);
+        cluster.on('disconnect', (w) => {
+            --onlineWorkers;
+            if(onlineWorkers <= 0) {
+                this.emit('end', {type: 'error', data: 'All workers exited.'});
+            }
+        });
 
         // Spawn workers
         for(var i = 0; i < this.workers; ++i) {
-            cluster.fork();
+            cluster.fork().on('worker-result',
+                              this.workerReturnedResult.bind(this));
         }
+    }
+
+    private process(): void {
+        // Open/create output file
+        fs.open(this.outpath, 'w', (err, fd) => {
+            if(err) {
+                this.emit('end', {
+                    type: 'error',
+                    data: 'Failed to open output file: ' + this.outpath
+                });
+
+                return;
+            }
+
+            this.outputFile = fd;
+            this.processDir(this.sdkpath);
+        });
+
+        // Signal workers to start processing
+        Object.keys(cluster.workers).forEach((id) => cluster.workers[id].send({
+            type: 'start-processing'
+        }));
     }
 
     private processDir(dir: string): void {
@@ -66,22 +98,40 @@ class AsdkTypedApp extends event.EventEmitter {
             var i: number, fullName: string;
 
             if(err) {
-                this.emit('error', err);
+                this.emit('error', {data: err});
                 return;
             }
 
             for(i = 0; i < files.length; ++i) {
-                fullName = path.join(dir, files[i]);
+                fullName = path.resolve(dir, files[i]);
                 fs.stat(fullName, (err, stats) => {
                     if(stats.isDirectory()) {
                         this.processDir(fullName);
                         return;
                     }
 
-                    this.getWorker().send({fileName: fullName});
+                    ++this.pendingItems;
+                    this.getWorker().send({type: 'errand', fileName: fullName});
                 });
             }
         });
+    }
+
+    private workerReturnedResult(result: {err: string; result: NodeBuffer}): void {
+        --this.pendingItems;
+
+        if(result.err) {
+            this.emit('error', {data: 'Worker returned error: ' + result.err});
+        } else {
+            fs.writeSync(this.outputFile, result.result, 0,
+                         result.result.length, null);
+        }
+
+
+        if(this.pendingItems <= 0) {
+            fs.closeSync(this.outputFile);
+            this.emit('end');
+        }
     }
 
     private getWorker(): cluster.Worker {
--- src/worker.ts
+++ src/worker.ts
@@ -1,10 +1,123 @@
 /// <reference path="../ts.d/node.d.ts"/>
 /// <reference path="../ts.d/linestream.d.ts"/>
 
+// @class, @constructor, @method, @property, @prop, @event
 import fs = require('fs');
+import path = require('path');
 
 import LineStream = require('linestream');
 
-process.on('message', (msg) => {
+import FileProcessor = require('./fileprocessor');
 
+var fileNamesToHandle: string[] = [],
+    sdkPath = path.resolve(process.argv.slice(-1).pop()),
+    apiStart = /^<api\s+name="([^"]+)">$/,
+    apiEnd = /^<\/api>$/,
+    param = /^@param\s+(\w+)\s+\{([^}]+)\}$/,
+    optParam = /^@param\s+\[(\w+)(?:="([^"]+)")?\]\s+\{([^}]+)\}$/,
+    property = /^@property\s+\{([^}]+)\}$/,
+    prop = /^\s+@prop\s+(\w+)\s+\{([^}]+)\}$/,
+    optProp = /^\s+@prop\s+\[(\w+)(?:="([^"]+)")?\]\s+\{([^}]+)\}$/,
+    apiKind = /^@(class|constructor|method)$/;
+
+
+process.on('message', (msg) => {
+    switch(msg.type) {
+        case "errand":
+            handleErrand(msg.fileName);
+            break;
+        case "start-processing":
+            handleStartProcessing();
+            break;
+        default:
+            process.send('worker-result', {
+                err: 'Unknown message type: ' + msg.type
+            });
+    }
 });
+
+/**
+ * Extracts and returns module name for its ambient declaration from given
+ * full file name. If fails to extract module name, returns null.
+ */
+function moduleNameFromFileName(fileName: string): string {
+    var modName: string = fileName.split(sdkPath).pop();
+
+    if(!modName) {
+        return null;
+    }
+
+    modName = fileName.replace('\\', '/');
+    // Loose leading '/' and .md'
+    if(path.extname(modName).toLowerCase() !== '.md') {
+        return null;
+    } else {
+        modName = modName.substr(0, modName.length - 3);
+    }
+
+    if(modName[0] === '/') {
+        modName = modName.slice(1);
+    }
+
+    return modName;
+}
+
+function handleErrand(fileName: string): void {
+    if(fileName && fileName.trim()) {
+        fileNamesToHandle.push(fileName);
+    }
+}
+
+function handleStartProcessing(): void {
+    fileNamesToHandle.forEach(processFile);
+}
+
+function processFile(fileName: string): void {
+    var ls: LineStream = new LineStream(),
+        is: fs.ReadStream = fs.createReadStream(fileName),
+        readErrors: string[] = [],
+        moduleName = moduleNameFromFileName(fileName),
+        fileProcessor = new FileProcessor(moduleName);
+
+    ls.on('lines', (lines: string[]) => {
+        lines.forEach((line: string) => {
+            var match;
+
+            if(match = apiStart.exec(line)) {
+                fileProcessor.apiStart(match[1]);
+            } else if(apiEnd.test(line)) {
+                fileProcessor.apiEnd();
+            } else if(match = param.exec(line)) {
+                fileProcessor.param.apply(fileProcessor, match.slice(1));
+            } else if(match = optParam.exec(line)) {
+                fileProcessor.optParam.apply(fileProcessor, match.slice(1));
+            } else if(match = property.exec(line)) {
+                fileProcessor.property(match[1]);
+            } else if(match = prop.exec(line)) {
+                fileProcessor.prop.apply(fileProcessor, match.slice(1));
+            } else if(match = optProp.exec(line)) {
+                fileProcessor.optProp.apply(fileProcessor, match.slice(1));
+            } else if(match = apiKind.exec(line)) {
+                fileProcessor[match[1] + 'Def']();
+            }
+        });
+    }).on('error', (err: string) => {
+        readErrors.push(err);
+    }).on('finish', () => {
+        var errors: string,
+            processingErrors: string[] = fileProcessor.errors();
+
+        if(readErrors.length > 0 || processingErrors.length > 0) {
+            errors = 'Enrountered ' + readErrors.length + ' and ' +
+                     processingErrors.length + ' while processing input file ' +
+                     fileName;
+        }
+
+        process.send('worker-result', {
+            err: errors,
+            result: fileProcessor.generate()
+        });
+    });
+
+    is.pipe(ls);
+}
