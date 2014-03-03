282b9ad3d6dd8e9a3236a7fec6ac64d6653873a6
vtomilin <vtomilin@myopera.com>
2014-03-03 17:21:02 -0500

    replaced memory-streams with own implementation

---
+++ src/bufferstream.ts
@@ -0,0 +1,63 @@
+/// <reference path="../ts.d/node.d.ts"/>
+
+import stream = require('stream');
+
+/**
+ * @class
+ * BufferStream implements stream.Writable such that the output is accumulated
+ * in memory until reclaimed with @{link #toBuffer}.
+ */
+ export class BufferStream extends stream.Writable {
+    /**
+     * Obtains Buffer object, filled with data, previously written to this
+     * stream. Then releases memory, purging locally accumulated data. Returns
+     * buffer object, containing the data.
+     */
+    toBuffer(): NodeBuffer {
+        var buffer = this.getBuffer();
+
+        this.buffers.length = 0; // Release memory
+
+        return buffer;
+    }
+
+    /**
+     * @returns a new Buffer object, filled with data, written to this stream.
+     */
+    getBuffer(): NodeBuffer {
+        var i: number = 0,
+            buffer: NodeBuffer = new Buffer(this.sizeBytes),
+            offset: number = 0;
+
+        for(i = 0; i < this.buffers.length; ++i) {
+            this.buffers[i].copy(buffer, offset);
+            offset += this.buffers[i].length;
+        }
+
+        return buffer;
+    }
+
+    /**
+     * @returns currently help size of data in bytes.
+     */
+    get length() {
+        return this.sizeBytes;
+    }
+
+    //------------------------------------------------- Supertype implementation
+    _write(data: any, encoding: string, callback: Function): void {
+        if(typeof data === 'string') {
+            data = new Buffer(data, encoding);
+        }
+
+        this.buffers.push(data);
+        this.sizeBytes += data.length;
+
+        callback(null);
+    }
+
+
+    //---------------------------------------------------------- Private members
+    private sizeBytes: number = 0;
+    private buffers: NodeBuffer[] = [];
+ }
--- src/definition.ts
+++ src/definition.ts
@@ -1,7 +1,17 @@
 /// <reference path="../ts.d/node.d.ts"/>
+/// <reference path="../ts.d/memory-streams.d.ts"/>
+import stream = require('stream');
+import bufstream = require('./bufferstream');
 
+/**
+ * @class
+ * Declares a protocol for processing abstract definition, to be implemented by
+ * concrete ones.
+ */
 export interface Definition {
+
     generate(): NodeBuffer;
+    generate(stream.Writable): void;
 
     errors(): string[];
 
@@ -18,6 +28,55 @@ export interface Definition {
     constructorDef(): void;
     methodDef(): void;
 }
+
+class CompoundDefinition implements Definition {
+    Definition definitions[] = [];
+    Definition nested;
+    apiName: string;
+
+    generate(): NodeBuffer {
+        var ostr = new bufstream.BufferStream();
+
+        this.generate(ostr);
+
+        return ostr.toBuffer();
+    }
+
+    errors(): string[];
+
+    apiStart(name: string): void {
+        if(!this.apiName) {
+            // Local definition is to follow
+            this.apiName = name;
+        } else {
+            // Nested definition
+            this.nested = new ForwardDefinition();
+            this.nested.apiStart(name);
+        }
+    }
+
+    apiEnd(): void {
+        if(this.nested) {
+            this.nested.apiEnd();
+            definitions.push(this.nested);
+
+            this.nested = null;
+        }
+
+        this.apiName = null;
+    }
+
+    param(name: string, type: string): void;
+    optParam(name: string, initVal: string, type: string): void;
+    property(type: string): void;
+    prop(name: string, type: string): void;
+    optProp(name: string, initVal: string, type: string): void;
+
+    classDef(): void;
+    constructorDef(): void;
+    methodDef(): void;
+}
+
 
 export class ClassDefinition implements Definition {
 
